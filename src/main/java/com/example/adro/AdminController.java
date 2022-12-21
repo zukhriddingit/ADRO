67cf290f9fd7a5c32908cb256591fe25fd730238
Sanjar Totliboyev <2110110@newuzbekistanuniversity.uz>
2022-12-21 09:56:16 +0500

    from Sanjar

---
+++ src/main/java/com/example/adro/AdminController.java
@@ -0,0 +1,32 @@
+package com.example.adro;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+
+    private String title;
+
+    private String description;
+
+    private String theaterName;
+
+    private String genre;
+
+    private String language;
+
+    private String country;
+
+    private Date releaseDate;
+
+    private double price;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
