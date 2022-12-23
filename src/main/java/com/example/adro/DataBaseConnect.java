7c76f489e2f23da796f5739158cde4b3843b1573
Asilbek Boboev <2110068@newuzbekistanuniversity.uz>
2022-12-23 10:13:45 +0500

    starting demo Asilbek

---
+++ src/main/java/com/example/adro/DataBaseConnect.java
@@ -0,0 +1,4 @@
+package com.example.adro;
+
+public class DataBaseConnect {
+}
--- src/main/java/com/example/adro/SignUpPageController.java
+++ src/main/java/com/example/adro/SignUpPageController.java
@@ -2,12 +2,15 @@ package com.example.adro;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class SignUpPageController {
 
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField fullname,email,phoneNum,userName,password,ConfirmPassword;
 
     @FXML
     protected void onHelloButtonClick() {
