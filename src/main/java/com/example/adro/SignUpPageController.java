a6f77de25b362095c9d8b818642b605ec530a025
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-01-02 23:51:16 +0500

    register db 2

--- src/main/java/com/example/adro/SignUpPageController.java
+++ src/main/java/com/example/adro/SignUpPageController.java
@@ -1,7 +1,9 @@
 package com.example.adro;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -11,14 +13,26 @@ public class SignUpPageController {
 
     @FXML
     private Label welcomeText;
+
     @FXML
-    private TextField fullname,dateOfBirth, email,phoneNum,userName,password,ConfirmPassword;
+    private TextField fullname;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phoneNum;
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private DatePicker dateOfBirth;
 
     @FXML
     protected void registerAction() throws SQLException {
 
         DataBaseConnect dataBaseConnect = new DataBaseConnect();
-        String sql = "INSERT INTO `register`(`fullname`, `email`, `phone`, `username`, `password`) VALUES ('"+fullname.getText()+"','"+email.getText()+"','"+phoneNum.getText()+"','"+userName.getText()+"','"+password.getText()+"')";
+        String sql = "INSERT INTO `register`(`fullname`, `email`, `phone`, `username`, `password`, `dateOfBirth`) VALUES ('"+fullname.getText()+"','"+email.getText()+"','"+phoneNum.getText()+"','"+userName.getText()+"','"+password.getText()+"','"+dateOfBirth.getValue()+"')";
         dataBaseConnect.insertData(sql);
     }
 
