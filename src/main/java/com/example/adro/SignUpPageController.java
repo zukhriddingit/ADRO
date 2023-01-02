ee7a06892f4fc79c9cae1c4c26eb26c35a593f62
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-01-02 20:27:35 +0500

    SignUp db 1

--- src/main/java/com/example/adro/SignUpPageController.java
+++ src/main/java/com/example/adro/SignUpPageController.java
@@ -5,16 +5,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import java.sql.SQLException;
+
 public class SignUpPageController {
 
     @FXML
     private Label welcomeText;
     @FXML
-    private TextField fullname,email,phoneNum,userName,password,ConfirmPassword;
+    private TextField fullname,dateOfBirth, email,phoneNum,userName,password,ConfirmPassword;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void registerAction() throws SQLException {
+
+        DataBaseConnect dataBaseConnect = new DataBaseConnect();
+        String sql = "INSERT INTO `register`(`fullname`, `email`, `phone`, `username`, `password`) VALUES ('"+fullname.getText()+"','"+email.getText()+"','"+phoneNum.getText()+"','"+userName.getText()+"','"+password.getText()+"')";
+        dataBaseConnect.insertData(sql);
     }
 
 }
