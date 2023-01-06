a377ca4db15688b32eae8ad150d1d4fcb3013251
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-01-06 21:42:55 +0500

    SignUp almost final)

--- src/main/java/com/example/adro/SignUpPageController.java
+++ src/main/java/com/example/adro/SignUpPageController.java
@@ -1,12 +1,18 @@
 package com.example.adro;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class SignUpPageController {
@@ -29,11 +35,18 @@ public class SignUpPageController {
     private DatePicker dateOfBirth;
 
     @FXML
-    protected void registerAction() throws SQLException {
+    protected void registerAction(ActionEvent event) throws SQLException, IOException {
 
         DataBaseConnect dataBaseConnect = new DataBaseConnect();
         String sql = "INSERT INTO `register`(`fullname`, `email`, `phone`, `username`, `password`, `dateOfBirth`) VALUES ('"+fullname.getText()+"','"+email.getText()+"','"+phoneNum.getText()+"','"+userName.getText()+"','"+password.getText()+"','"+dateOfBirth.getValue()+"')";
         dataBaseConnect.insertData(sql);
+        Node node = (Node)event.getSource();
+        Stage dialogStage = (Stage) node.getScene().getWindow();
+        dialogStage.close();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Dashboard.fxml")));
+        dialogStage.setScene(scene);
+        dialogStage.show();
+
     }
 
 }
