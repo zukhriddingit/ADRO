8bbfbfb9e217c1f963db48798f8a072a4a08a4e9
Zukhriddin Fakhriddinov <faxriddinovzuxriddin60@gmail.com>
2023-01-07 21:29:57 +0500

    feat: modified profile fxml

--- src/main/java/com/example/adro/MyProfileController.java
+++ src/main/java/com/example/adro/MyProfileController.java
@@ -3,6 +3,8 @@ package com.example.adro;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class MyProfileController {
 
@@ -13,6 +15,30 @@ public class MyProfileController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private Label profile_age;
+
+    @FXML
+    private Button profile_editbtn;
+
+    @FXML
+    private Label profile_email;
+
+    @FXML
+    private Label profile_name;
+
+    @FXML
+    private Label profile_password;
+
+    @FXML
+    private Label profile_phonenumber;
+
+    @FXML
+    private ImageView profile_picture;
+
+    @FXML
+    private Label profile_username;
     public void myProfile(){
         Label name = new Label();
         name.setText("SALJ");
