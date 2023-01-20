c618526118feec9ddf6a0dc955065fb69142988c
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-01-20 08:47:29 +0500

    changes for DataBaseConnect

--- src/main/java/com/example/adro/DataBaseConnect.java
+++ src/main/java/com/example/adro/DataBaseConnect.java
@@ -2,6 +2,7 @@ package com.example.adro;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,7 +10,7 @@ import java.util.logging.Logger;
 public class DataBaseConnect {
     private static String HOST = "127.0.0.1";
     private static int PORT = 3306;
-    private static String DB_NAME = "javafx";
+    private static String DB_NAME = "adro";
     private static String USERNAME = "root";
     private static String PASSWORD = "";
     private static Connection connection;
@@ -22,5 +23,14 @@ public class DataBaseConnect {
         }
 
         return  connection;
+    }
+
+    public static void insertData(String sql) throws SQLException {
+        Statement statement = getConnect().createStatement();
+        if (statement.executeUpdate(sql)>0){
+            System.out.println("Successfully added!");
+        }else{
+            System.out.println("Something went wrong!!!");
+        }
     }
 }
