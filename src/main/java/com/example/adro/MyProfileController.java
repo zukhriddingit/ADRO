a50975d1149d10b62aa6accaea2cd2a44dfecde0
Asilbek Boboev <2110068@newuzbekistanuniversity.uz>
2023-01-27 20:26:32 +0500

    from Asilbek

--- src/main/java/com/example/adro/MyProfileController.java
+++ src/main/java/com/example/adro/MyProfileController.java
@@ -8,42 +8,5 @@ import javafx.scene.image.ImageView;
 
 public class MyProfileController {
 
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    private Label profile_age;
-
-    @FXML
-    private Button profile_editbtn;
-
-    @FXML
-    private Label profile_email;
-
-    @FXML
-    private Label profile_name;
-
-    @FXML
-    private Label profile_password;
-
-    @FXML
-    private Label profile_phonenumber;
-
-    @FXML
-    private ImageView profile_picture;
-
-    @FXML
-    private Label profile_username;
-    public void myProfile(){
-        Label name = new Label();
-        name.setText("SALJ");
-        Label dateOfBirth = new Label();
-        dateOfBirth.setText("Date of birth");
-        VBox vBox = new VBox();
-    }
 }
