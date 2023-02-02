829507bf8531e1c744002c8db2ff8f50292e9674
Asilbek Boboev <2110068@newuzbekistanuniversity.uz>
2023-02-02 17:02:09 +0500

    Login page new one by Asilbek updated

--- src/main/java/com/example/adro/SignInController.java
+++ src/main/java/com/example/adro/SignInController.java
@@ -3,22 +3,34 @@ package com.example.adro;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class SignInController {
+public class SignInController implements Initializable {
 
     @FXML
     private TextField username;
     @FXML
     private PasswordField password;
+    @FXML
+    private HBox hbox;
+
+
     public void signInButton(ActionEvent event) throws IOException, SQLException {
         DataBaseConnect db = new DataBaseConnect();
         if (username.getText().isEmpty()){
@@ -35,6 +47,22 @@ public class SignInController {
         } else {
             System.out.println("another error!");
         }
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File file = new File("src/main/java/pictures");
+        ImageView imageView =new ImageView();
+        try {
+            Image image = new Image(file.toURI().toURL().toString()+"photo_2023-02-02_16-57-58.jpg");
+            imageView.setFitHeight(463);
+            imageView.setFitWidth(350);
+            imageView.setImage(image);
+            hbox.getChildren().add(imageView);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
